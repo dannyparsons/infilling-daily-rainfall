df70b9c7017f560d4e4608f8e030250a847adaf1
Danny Parsons <danny@idems.international>
2025-07-22 18:21:34 +0100

    implemented LOCI method

--- src/methods.R
+++ src/methods.R
@@ -7,14 +7,37 @@ direct_replace <- function(obs, est) {
 }
 
 # Threshold adjustment
-thresh_calc <- function(obs, est, obs_thresh = 0.85) {
+thresh <- function(obs, est, obs_thresh = 0.85) {
   id <- !is.na(obs) & !is.na(est)
   p_obs <- mean(obs[id] > obs_thresh, na.rm = TRUE)
   quantile(est[id], 1 - p_obs)[[1]]
 }
 
+s_loci <- function(obs, est, obs_thresh = 0.85, est_thresh = NULL) {
+  if (is.null(est_thresh)) est_thresh <- thresh(obs, est, obs_thresh)
+  id <- !is.na(obs) & !is.na(est)
+  s_obs <- mean(obs[id & obs > obs_thresh]) - obs_thresh
+  s_est <- mean(est[id & est > est_thresh]) - est_thresh
+  s <- s_obs / s_est
+  s
+}
+
 # Local Intensity Scaling
+loci <- function(obs, est, obs_thresh = 0.85) {
+  est_thresh <- thresh(obs, est, obs_thresh)
+  s <- s_loci(obs, est, obs_thresh, est_thresh)
+  est_loci <- obs_thresh + s * (est - est_thresh)
+  est_loci <- pmax(est_loci, 0)
+  est_loci
+}
 
 # Quantile Mapping
+fit_gamma <- function(obs, obs_thresh = 0.85, method = "mle") {
+  obs <- obs[obs > obs_thresh] - obs_thresh
+  obs <- as.numeric(na.omit(obs))
+  if(length(obs) > 5) fitdistrplus::fitdist(obs, distr = "gamma", method = method)
+  else NULL
+}
 
 # Weather generator
+
--- src/testing.R
+++ src/testing.R
@@ -1,13 +1,17 @@
 library(here)
 library(readr)
 library(dplyr)
+library(fitdistrplus)
 
 set.seed(12)
 
 x <- c(0, 0, 5, 6, 2, NA, 2, 7, 0.8, 0.2, 0.9, 6, 30, 50, NA, 3, 0, 0, NA)
 y <- c(0, 5, 7, 7, 1, 8,  2, 8, 1.8, 6.5, 4.1, 9, 12, 20, 3,  8, 2, 8, 25)
 
-thresh_calc(x, y)
+source(here("src", "methods.R"))
+
+thresh(x, y)
+loci(x, y)
 
 rwanda <- readr::read_csv(here("data", "rwanda_daily.csv"))
 kigali <- rwanda %>%
